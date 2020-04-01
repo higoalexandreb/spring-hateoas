b3d515363546932431eb2e7fd658c3ca07b6456d
Higo Alexandre <higoalexandreb@gmail.com>
2020-03-31 22:51:37 -0300

    Added model

---
+++ src/main/java/com/higodev/contacts/models/Contact.java
@@ -0,0 +1,35 @@
+package com.higodev.contacts.models;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.hateoas.ResourceSupport;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity(name="contacts")
+public class Contact extends ResourceSupport implements Serializable{
+	private static final long serialVersionUID = -6827773046529365962L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private UUID contactId;
+	
+	@Column(nullable = false, length = 100)
+	private String name;
+	
+	@Column(length = 11)
+	private String phone;
+	
+	@Column(length = 100, unique = true)
+	private String email;
+}
